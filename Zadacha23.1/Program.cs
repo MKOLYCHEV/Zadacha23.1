4d6381e3aa3355b851195b276c9637b75bf25e4d
Миша Мишин <spawn.rapture-0n@icloud.com>
2021-12-19 16:52:01 +0300

    Добавьте файлы проекта.

---
+++ Zadacha23.1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Zadacha23._1
+{
+    class Program
+    {
+        static int num = 0;
+        static int factorialNum = 1;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число для определения факториала: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            if (num <= 0)
+            {
+                Console.WriteLine("Ошибка! Число должно быть больше нуля.");
+                Console.ReadKey();
+                return;
+            }
+            else
+            {
+                FactorialAsync();
+            }
+
+            Console.WriteLine("Метод Main закончил работу.");
+            Console.ReadKey();
+        }
+        static async Task FactorialAsync()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    factorialNum *= i;
+                    Console.WriteLine($"Факториал числа {i} равен {factorialNum}.");
+                    Thread.Sleep(100);
+                }
+            });
+        }
+    }
+}
